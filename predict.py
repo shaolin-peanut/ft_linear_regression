0ab1c0d8510a72059fbb22fc7816124d56a40dfb
shaolin-peanut <shaolinpeanut@gmail.com>
2024-09-26 18:13:33 +0200

    better syntax

--- predict.py
+++ predict.py
@@ -1,12 +1,11 @@
-def predict(slope, intercept, mileage):
-    return (slope * mileage) + intercept
 
 def main():
     mileage = float(input("Enter a mileage: "))
     with open("theta", 'r') as file:
         theta = file.read().strip().split(',')
-    prediction = predict(float((theta[1])), float(theta[0]), mileage);
-    print("Predicted price is: ", prediction);
+    slope = float(theta[1])
+    intercept = float(theta[0])
+    print("Predicted price is: ", (slope * mileage) + intercept)
 
 if __name__ == '__main__':
     main()
